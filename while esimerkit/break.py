f28e7216740fba79b17973d0090667ade78dad46
Enni Kurki <ennikur@metropolia.fi>
2025-03-01 11:03:09 +0200

    ok

---
+++ while esimerkit/break.py
@@ -0,0 +1,7 @@
+komento = input ("Anna komento: ")
+while komento!="lopeta":
+    if komento=="MAYDAY":
+        break
+    print ("Suoritan toiminnon: " + komento)
+    komento = input("Anna komento: ")
+print ("Toiminnot lopetettu.")
